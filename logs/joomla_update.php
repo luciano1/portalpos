4004fa08c588b43ddab09c5f3d0640b261a0042a
Luciano Antunes Rocha <lucianoantunesrocha@gmail.com>
2016-10-11 16:29:47 -0300

    Subir

---
+++ logs/joomla_update.php
@@ -0,0 +1,49 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-11-13 17:29:57 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-11-13T17:29:57+00:00	INFO ::1	update	Update started by user Super User (311). Old version is 3.4.3.
+2015-11-13T17:29:57+00:00	INFO ::1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.5/Joomla_3.4.x_to_3.4.5-Stable-Patch_Package.zip.
+2015-11-13T17:30:04+00:00	INFO ::1	update	File Joomla_3.4.x_to_3.4.5-Stable-Patch_Package.zip successfully downloaded.
+2015-11-13T17:30:04+00:00	INFO ::1	update	Starting installation of new version.
+2015-11-13T17:30:17+00:00	INFO ::1	update	Finalising installation.
+2015-11-13T17:30:17+00:00	INFO ::1	update	Deleting removed files and folders.
+2015-11-13T17:30:23+00:00	INFO ::1	update	Cleaning up after installation.
+2015-11-13T17:30:23+00:00	INFO ::1	update	Update to version 3.4.5 is complete.
+2016-02-05T16:58:58+00:00	INFO ::1	update	Update started by user Super User (311). Old version is 3.4.5.
+2016-02-05T16:58:58+00:00	INFO ::1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.8/Joomla_3.4.x_to_3.4.8-Stable-Patch_Package.zip.
+2016-02-05T16:59:04+00:00	INFO ::1	update	File Joomla_3.4.x_to_3.4.8-Stable-Patch_Package.zip successfully downloaded.
+2016-02-05T16:59:04+00:00	INFO ::1	update	Starting installation of new version.
+2016-02-05T16:59:24+00:00	INFO ::1	update	Finalising installation.
+2016-02-05T16:59:24+00:00	INFO ::1	update	Deleting removed files and folders.
+2016-02-05T17:00:08+00:00	INFO ::1	update	Cleaning up after installation.
+2016-02-05T17:00:08+00:00	INFO ::1	update	Update to version 3.4.8 is complete.
+2016-04-08T18:27:08+00:00	INFO ::1	update	Update started by user Super User (311). Old version is 3.4.8.
+2016-04-08T18:27:09+00:00	INFO ::1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.5.1/Joomla_3.5.1-Stable-Update_Package.zip.
+2016-04-08T18:27:19+00:00	INFO ::1	update	File Joomla_3.5.1-Stable-Update_Package.zip successfully downloaded.
+2016-04-08T18:27:20+00:00	INFO ::1	update	Starting installation of new version.
+2016-04-08T18:28:05+00:00	INFO ::1	update	Finalising installation.
+2016-04-08T18:28:06+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-07-01. Query text: ALTER TABLE `#__session` MODIFY `session_id` varchar(191) NOT NULL DEFAULT '';.
+2016-04-08T18:28:07+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-07-01. Query text: ALTER TABLE `#__user_keys` MODIFY `series` varchar(191) NOT NULL;.
+2016-04-08T18:28:07+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-10-13. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2016-04-08T18:28:07+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-10-26. Query text: ALTER TABLE `#__contentitem_tag_map` DROP INDEX `idx_tag`;.
+2016-04-08T18:28:08+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-10-26. Query text: ALTER TABLE `#__contentitem_tag_map` DROP INDEX `idx_type`;.
+2016-04-08T18:28:08+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-10-30. Query text: UPDATE `#__menu` SET `title` = 'com_contact_contacts' WHERE `id` = 8;.
+2016-04-08T18:28:08+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-11-04. Query text: DELETE FROM `#__menu` WHERE `title` = 'com_messages_read' AND `client_id` = 1;.
+2016-04-08T18:28:08+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-11-04. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2016-04-08T18:28:08+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-11-05. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2016-04-08T18:28:08+00:00	INFO ::1	update	Ran query from file 3.5.0-2015-11-05. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2016-04-08T18:28:09+00:00	INFO ::1	update	Ran query from file 3.5.0-2016-02-26. Query text: CREATE TABLE IF NOT EXISTS `#__utf8_conversion` (   `converted` tinyint(4) NOT N.
+2016-04-08T18:28:09+00:00	INFO ::1	update	Ran query from file 3.5.0-2016-02-26. Query text: INSERT INTO `#__utf8_conversion` (`converted`) VALUES (0);.
+2016-04-08T18:28:09+00:00	INFO ::1	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` DROP INDEX `idx_link_old`;.
+2016-04-08T18:28:09+00:00	INFO ::1	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` MODIFY `old_url` VARCHAR(2048) NOT NULL;.
+2016-04-08T18:28:10+00:00	INFO ::1	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` MODIFY `new_url` VARCHAR(2048) NOT NULL;.
+2016-04-08T18:28:11+00:00	INFO ::1	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` MODIFY `referer` VARCHAR(2048) NOT NULL;.
+2016-04-08T18:28:11+00:00	INFO ::1	update	Ran query from file 3.5.0-2016-03-01. Query text: ALTER TABLE `#__redirect_links` ADD INDEX `idx_old_url` (`old_url`(100));.
+2016-04-08T18:28:12+00:00	INFO ::1	update	Ran query from file 3.5.1-2016-03-25. Query text: ALTER TABLE `#__user_keys` MODIFY `user_id` varchar(150) NOT NULL;.
+2016-04-08T18:28:12+00:00	INFO ::1	update	Ran query from file 3.5.1-2016-03-29. Query text: UPDATE `#__utf8_conversion` SET `converted` = 0  WHERE (SELECT COUNT(*) FROM `#_.
+2016-04-08T18:28:12+00:00	INFO ::1	update	Deleting removed files and folders.
+2016-04-08T18:29:10+00:00	INFO ::1	update	Cleaning up after installation.
+2016-04-08T18:29:10+00:00	INFO ::1	update	Update to version 3.5.1 is complete.
